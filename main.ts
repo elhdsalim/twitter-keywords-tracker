d53d02fe23b72f6e1910dff72c43d7a6224adeef
slmhd <salim.elhadad.etu@univ-lille.fr>
2026-02-25 12:57:21 +0100

    remove rettiwt logger

--- main.ts
+++ main.ts
@@ -14,7 +14,6 @@ import { Monitor } from "./classes/Monitor";
 
     const rettiwt = new Rettiwt({
         apiKey: API_KEY,
-        logging: true
     });
 
     const m = new Monitor("./tweets.json", rettiwt, WORD, WEBHOOK_URL)
